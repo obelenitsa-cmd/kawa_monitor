a05bef627b53ef1e79f30a81b71a12c3365671e5
obelenitsa-cmd <obelenitsa@gmail.com>
2026-05-16 09:54:52 +0300

    display1.47.ino

--- kawa_monitor/display1.47.ino
+++ kawa_monitor/display1.47.ino
@@ -146,15 +146,15 @@ void GearDisplay(uint8_t gear)
    gfx->setCursor(103, 138);
    gfx->setFont(&DotDigital12); //шрифт
    gfx->setTextSize(4 /* x scale */, 4 /* y scale */, 4 /* pixel_margin */); 
-      if (geardata==0) {
+      if (gear==0) {
         gfx->setTextColor(GREEN, BLACK);
         gfx->print("N");
       }
-      else if (geardata>0 && geardata<10){
+      else if (gear>0 && gear<10){
         gfx->setTextColor(WHITE, BLACK);
         gfx->print(gear);
       } 
-      else if (geardata==45) {
+      else if (gear==45) {
         gfx->setTextColor(WHITE, BLACK);
         gfx->print("-");
       }
