630c84a2e7bc7d76a7056361c94fbba2e2ecb2f6
obelenitsa-cmd <obelenitsa@gmail.com>
2026-07-02 09:49:55 +0300

    kawa_monitor_1.47.ino

--- kawa_monitor_1.47/kawa_monitor_1.47.ino
+++ kawa_monitor_1.47/kawa_monitor_1.47.ino
@@ -22,7 +22,7 @@ void setup() {
   SetupOTAWiFi();
   SetupBLE_TPMS();
 
-  xTaskCreatePinnedToCore(Task1code, "Task1", 4096, NULL, 1, &Task1, 0 ); //Ядро 0 
+  xTaskCreatePinnedToCore(Task1code, "Task1", 8192, NULL, 1, &Task1, 0 ); //Ядро 0 (4096 - периодически может происходить переполнение)
   delay(1);
   xTaskCreatePinnedToCore(Task2code, "Task2", 6144, NULL, 1, &Task2, 1); //Ядро 1  
 }
